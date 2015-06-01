5b331d6bec73962715ba9630893aaf454b1942ae
Mohamed Boudra <boudra.moha@gmail.com>
2015-06-02 01:22:41 +0200

    Coses 2.0

--- html/forum/db.inc.php
+++ html/forum/db.inc.php
@@ -8,7 +8,7 @@
     try {
         $opc = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
         $dsn = "mysql:host=localhost;dbname=forum";
-        $conn = new PDO($dsn, "root", "", $opc);
+        $conn = new PDO($dsn, "root", "1234", $opc);
         $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, true);
     }
     catch (PDOException $e) {
