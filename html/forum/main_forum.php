167aaa4e43a22ab45aa59efe2de84c6685cb910b
Mohamed Boudra <boudra.moha@gmail.com>
2015-06-02 00:51:49 +0200

    Modifició de permisos i rutes

---
+++ html/forum/main_forum.php
@@ -0,0 +1,29 @@
+<?php 
+	require('db.inc.php'); 
+	require('functions.php'); 
+?>
+
+<!DOCTYPE html>
+<html>
+<?php head("Forum"); ?>
+<body>
+
+	<div class="panel">
+		<?php 
+			mainForum($conn);
+			$conn=null;
+		?>
+	</div>
+
+	<script type="text/javascript">
+		$(document).ready(function(){
+			$('.panel').width($(window).width()-75);
+			$(window).resize(function(){
+				$('.panel').width($(window).width()-75);
+			});
+			
+		});
+	</script>
+
+</body>
+</html>
